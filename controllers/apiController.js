dc08e5e1dacd19f5f69dac384b6265bdea04fe22
Thomas Mylott <mylottiv@gmail.com>
2019-12-18 12:26:26 -0600

    Moved scraping logic to apiController. In process of implementing selective saving of scraped articles

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,4 +1,44 @@
-const Article = require('../models/article')
+const Article = require('../models/article');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function scrapeNews(cb) {
+    try {
+        let raw = await axios.get('http://www.nintendolife.com/news')
+        if (raw.status === 200) {
+            const $ = cheerio.load(raw.data);
+            let tempArticles = []
+            $('#listing- > div > ul > li[data-type="article"]').each(function(i) {
+                if (i <= 50) {
+                    let item = $(this).find('.item-wrap')
+                    let newArticle = {
+                        image: item.find('img').attr('src'),
+                        title: item.find('a .title').text(),
+                        link: item.find('.title, .accent-hover, a').attr('href'),
+                        description: item.find('.description').text(),
+                        content: item.find('.text').text()
+                    };
+                    tempArticles.push(newArticle);
+                }
+                else return false;
+            });
+            tempArticles = await Promise.all(tempArticles.map( async article => {
+                let test = await Article.findOne(article);
+                if (test !== null) {
+                    return Article.create(article);
+                }
+                else {
+                    return console.log('Article already in DB');
+                }
+            }));
+            let confirm = await Promise.all(tempArticles);
+            let results = await Article.find();
+            cb(results);
+        } else console.log('Status not 200');
+    } catch(err) {
+        console.log('Error', err);
+    }
+}
 
 module.exports = {
 
@@ -9,7 +49,8 @@ module.exports = {
     },
 
     scrapeNewArticles: (req, res) => {
-        console.log('dummy');
+        console.log('here')
+        scrapeNews(articles => res.json(articles))
     },
 
     getOneArticle: (req, res) => {
--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,45 +1,18 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 const Article = require('../models/article');
-
-async function scrapeNews(cb) {
-    try {
-        let raw = await axios.get('http://www.nintendolife.com/news')
-        if (raw.status === 200) {
-            const $ = cheerio.load(raw.data);
-            const tempArticles = []
-            $('#listing- > div > ul > li[data-type="article"]').each(function(i) {
-                if (i <= 10) {
-                    let item = $(this).find('.item-wrap')
-                    let newArticle = {
-                        image: item.find('img').attr('src'),
-                        title: item.find('a .title').text(),
-                        link: item.find('.title, .accent-hover, a').attr('href'),
-                        description: item.find('.description').text(),
-                        content: item.find('.text').text()
-                    };
-                    tempArticles.push(Article.create(newArticle));
-                }
-                else return false;
-            });
-            let confirm = await Promise.all(tempArticles);
-            let results = await Article.find();
-            cb(results);
-        } else console.log('Status not 200');
-    } catch(err) {
-        console.log('Error', err);
-    }
-}
 
 module.exports = {
     serveHome: (req, res) => {
-        scrapeNews(article => res.render('home',{article}));
-        
+        axios.put('/api/scrape', {}, { baseURL: 'http://localhost:8080' })
+        .then(result => {
+            let articles = result.data;
+            res.render('home',{articles})
+        }).catch(err => console.log(err));
     },
 
     serveSaved: (req, res) => {
         Article.find({ saved: true })
-        .then(article => res.render('saved', {article}))
+        .then(articles => res.render('saved', {articles}))
         .catch(err => console.log(err));
     }
 
